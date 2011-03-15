a2600b2ad2e8ade564d665bd2e52dc9a46ff7bdc
Sahil Verma <vermasque@gmail.com>
2011-03-15 16:18:26 -0400

    started using Robotium, completed song not set test

--- tests/src/org/vermasque/songalarm/test/SongAlarmActivityTest.java
+++ tests/src/org/vermasque/songalarm/test/SongAlarmActivityTest.java
@@ -2,14 +2,19 @@ package org.vermasque.songalarm.test;
 
 import org.vermasque.songalarm.SongAlarmActivity;
 
+import android.content.res.Resources;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.KeyEvent;
 
+import com.jayway.android.robotium.solo.Solo;
+
 public class SongAlarmActivityTest extends
 		ActivityInstrumentationTestCase2<SongAlarmActivity>
 {
+	private Solo solo;
+	
 	private SongAlarmActivity mActivity;
 	
 	private Preference mAlarmTimePref, mSongPref;
@@ -37,6 +42,8 @@ public class SongAlarmActivityTest extends
 		mAlarmEnabledPref = 
 			(CheckBoxPreference)mActivity.findPreference(SongAlarmActivity.PREF_ALARM_ENABLED);
 		mSongPref = mActivity.findPreference(SongAlarmActivity.PREF_SONG);	
+		
+		solo = new Solo(this.getInstrumentation(), mActivity);
 	}
 	
 	public void testPreConditions() 
@@ -45,25 +52,43 @@ public class SongAlarmActivityTest extends
 			mActivity.getResources().getString(
 				org.vermasque.songalarm.R.string.pref_summary_default);
 		
-		assertFalse(mActivity.isAlarmEnabled());
-		assertFalse(mAlarmEnabledPref.isChecked());
+		assertAlarmNotEnabled();
 		assertEquals(SUMMARY_DEFAULT, mAlarmTimePref.getSummary());
 		assertEquals(SUMMARY_DEFAULT, mSongPref.getSummary());
 	}
 	
 	public void testNoInputsSet()
 	{
-		mActivity.runOnUiThread(new Runnable()
-		{		
-			@Override
-			public void run()
-			{
-				mAlarmEnabledPref.getView(null, null).requestFocus();
-			}
-		});
+		attemptEnableAlarm();
 		
-		sendKeys(KeyEvent.KEYCODE_DPAD_CENTER);
-		
+		assertAlarmNotEnabled();
+	}
+
+	private void assertAlarmNotEnabled()
+	{
 		assertFalse(mActivity.isAlarmEnabled());
+		assertFalse(mAlarmEnabledPref.isChecked());
+	}
+
+	private void attemptEnableAlarm()
+	{
+		solo.clickOnCheckBox(0);
+	}
+	
+	public void testSongNotSet()
+	{
+		// Had to make a String from CharSequence
+		solo.clickOnText(mAlarmTimePref.getTitle().toString());
+		
+		// date_time_set is in com.android.internal.R based on TimePickerDialog
+		// source code.  The string is defined in 
+		// <sdk>/platforms/<insert platform here>/res/values/string.xml
+		solo.clickOnButton(
+			getInstrumentation().getContext().getResources().getString(
+				R.string.date_time_set));
+		
+		attemptEnableAlarm();
+		
+		assertAlarmNotEnabled();
 	}
 }
