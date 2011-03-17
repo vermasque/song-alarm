e39a38d5e42a273f8fc005a17a13d98b9a49ae99
Sahil Verma <vermasque@gmail.com>
2011-03-16 20:35:02 -0400

    added GPL header to AlarmTime source file, update SongAlarmActivity source file to reflect the fact that multiple source files now exist as part of this program

--- src/org/vermasque/songalarm/SongAlarmActivity.java
+++ src/org/vermasque/songalarm/SongAlarmActivity.java
@@ -1,5 +1,7 @@
 /**
- *  This program is free software: you can redistribute it and/or modify
+ *  This file is part of Song Alarm.
+ * 
+ *  Song Alarm is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
